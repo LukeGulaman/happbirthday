d559d5467682b5b093c3a90ddec79c5ca9ca5c04
Luke Gulaman <lukegulaman@gmail.com>
2025-05-18 17:20:37 +0800

    Another small fix

--- js/policyChecker.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("securitypolicyviolation", (e) => {
-  console.log(e.blockedURI);
-  console.log(e.violatedDirective);
-  console.log(e.originalPolicy);
-
-  console.log(e.sample);
-  console.log(`at ${e.lineNumber}:${e.columnNumber}`);
-});
