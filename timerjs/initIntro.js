974e6d937d610432f779de4a67f5ebb0cc982713
Luke Gulaman <lukegulaman@gmail.com>
2025-05-19 21:47:52 +0800

    Another fix

--- timerjs/initIntro.js
+++ timerjs/initIntro.js
@@ -2,12 +2,25 @@ const body = document.body;
 const welcomeText = document.getElementById("welcomeText");
 const timeDesc = document.getElementById("timedesc");
 const bgMusic = document.getElementById("bgMusic");
+const isMobile = window.mobileCheck();
+
+let cssHref = "pc";
+let hasClicked = false;
+
+if (isMobile) {
+    cssHref = "mobile"
+}
+
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = `timercss/style-${cssHref}.css`;
+head.appendChild(link);
 
 bgMusic.volume = 1.;
 bgMusic.loop = true;
-
-
-let hasClicked = false;
 
 wavyText(timeDesc, timeDesc.textContent, "wavy", 30);
 
@@ -17,8 +30,14 @@ function clickScreen(elem) {
     hasClicked = true;
     body.style.overflow = "hidden";
 
-    elem.style.width = "300vw";
-    elem.style.height = "300vw";
+    if (isMobile) {
+        elem.style.width = "500vw";
+        elem.style.height = "500vw";
+    } else {
+        elem.style.width = "300vw";
+        elem.style.height = "300vw";
+    }
+
     elem.style.backgroundColor = "rgba(255,255,255,0)";
 
     welcomeText.style.opacity = 0;
